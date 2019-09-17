a4ef47b7ab468617ed3080c8949316c2b62805b6
Soriyany <soriyanykeo@soriyanys-mbp.attlocal.net>
2019-09-16 19:30:59 -0500

    Create a project and Setup grouping by types

---
+++ NYTNews/NYTNews/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTNews
+//
+//  Created by Soriyany keo on 9/16/19.
+//  Copyright © 2019 Soriyany keo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
